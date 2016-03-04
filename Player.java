fd32669a3256f294b252d799522afe50573d6b01
jvcarolin <jvcarolin@gmail.com>
2016-03-03 20:41:24 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,6 +2,7 @@
  *Represents a player of the game. 
  */
 
+
 public abstract class Player {
 	
 	/**
@@ -36,14 +37,20 @@ public abstract class Player {
 		legalMoves = "";
 		numLegalMoves = 0;
 		
+//		for(int i = 1; i <= 8; i++){//Check rows
+//			for(int j = 1; j <= 8; j++){//Check cols
+//				if(r.isLegalMove)
+//			}//Check cols
+//		}//Check rows
+		
 		for(int i = 1; i <= 8; i++){
 			for(int j = 1; j <= 8; j++){
 				if(r.isLegalMove(i, j, this.getPlayerName())){
 					numLegalMoves ++;
-					legalMoves.concat(String.valueOf(i));
-					legalMoves.concat(String.valueOf(' '));
-					legalMoves.concat(String.valueOf(j));
-					legalMoves.concat(String.valueOf(','));
+					legalMoves = legalMoves.concat(String.valueOf(i));
+					legalMoves = legalMoves.concat(String.valueOf(' '));
+					legalMoves = legalMoves.concat(String.valueOf(j));
+					legalMoves = legalMoves.concat(String.valueOf(','));
 				}//if legal
 				
 			}// for loop j
@@ -54,7 +61,7 @@ public abstract class Player {
 		if(numLegalMoves <= 0){
 			System.out.println(this.getPlayerName() + " player has no legal moves.");
 		}//if no legal moves
-		
+				
 		return legalMoves;
 		
 	}//findLegalMoves(reversiBoard b)
@@ -89,10 +96,6 @@ public abstract class Player {
 	/**
 	 * Returns character representation of player's name.
 	 * Should be either 'X' or 'O'.
-	 * 
-	 * @author	John Carolin
-	 * @author	Sinna Uy
-	 * @return	Character representation of player's name
 	 */
 	public abstract char getPlayerName();
 	
@@ -102,7 +105,6 @@ public abstract class Player {
 	 * Determines player's move depending on what type of
 	 * Player object calls the method
 	 * 
-	 * @author		John Carolin
 	 * @param	r	Reversi object representing game in progress
 	 */
 	public abstract String getMove(Reversi r);
